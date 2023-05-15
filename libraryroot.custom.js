bcce759a424a23fd4e255817e0455f1c7bbeeedf
Hexality <victornacano@gmail.com>
2023-05-14 21:31:55 -0300

    fix js repetition for no reason

--- libraryroot.custom.js
+++ libraryroot.custom.js
@@ -1,30 +1,32 @@
-const delay = ms => new Promise(res => setTimeout(res, ms));
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
-const removeGarbage = async () => {
-    await delay(3000);
-    const remAddGamBut = document.querySelector('[class*="bottombar_AddGameButton_"]');
-    const remFriendsBut = document.querySelector('[class*="bottombar_FriendsButton_"]');
-    remAddGamBut.remove();
-    remFriendsBut.remove();
+sttgBut = document.querySelector('[class="steamSettingsButton"]');
+if (!sttgBut) {
+    const removeGarbage = async () => {
+            await delay(3000);
+            const remAddGamBut = document.querySelector('[class*="bottombar_AddGameButton_"]');
+        const remFriendsBut = document.querySelector('[class*="bottombar_FriendsButton_"]');
+        remAddGamBut.remove();
+        remFriendsBut.remove();
+    };
+
+    const addButtons = async () => {
+        await delay(1000);
+        const divSet = document.createElement("div");
+        divSet.classList.add("steamSettingsButton");
+        divSet.addEventListener("click", function () {location.href = "steam://settings";},false);
+        const divSetIcon = document.createElement("span");
+        divSetIcon.classList.add("steamSettingsIcon");
+        const setIcon = document.createTextNode("");
+        divSetIcon.appendChild(setIcon);
+        divSet.appendChild(divSetIcon);
+
+        const parent = document.querySelector('[class*="bottombar_BottomBar_"]');
+        const child = document.querySelector(
+            '[class*="bottombar_DownloadStatus_"]'
+        );
+        parent.insertBefore(divSet, child);
+    };
+    removeGarbage();
+    addButtons();
 }
-
-const addButtons = async () => {
-    await delay(3000);
-    const divSet = document.createElement("div");
-    divSet.classList.add("steamSettingsButton");
-    divSet.addEventListener('click', function() {
-        location.href = 'steam://settings'
-    }, false);
-    const divSetIcon = document.createElement("span");
-    divSetIcon.classList.add("steamSettingsIcon");
-    const setIcon = document.createTextNode("");
-    divSetIcon.appendChild(setIcon);
-    divSet.appendChild(divSetIcon);
-
-    const parent = document.querySelector('[class*="bottombar_BottomBar_"]');
-    const child = document.querySelector('[class*="bottombar_DownloadStatus_"]');
-    parent.insertBefore(divSet, child);
-}
-
-removeGarbage()
-addButtons()
