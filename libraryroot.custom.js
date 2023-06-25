99553628f49fed93f5a7dfb6609feb0ff86fe52e
Hexality <victornacano@gmail.com>
2023-06-24 23:45:20 -0300

    winui-rewrite

--- libraryroot.custom.js
+++ libraryroot.custom.js
@@ -1,32 +1,3 @@
-const delay = (ms) => new Promise((res) => setTimeout(res, ms));
-
-sttgBut = document.querySelector('[class="steamSettingsButton"]');
-if (!sttgBut) {
-    const removeGarbage = async () => {
-            await delay(3000);
-            const remAddGamBut = document.querySelector('[class*="bottombar_AddGameButton_"]');
-        const remFriendsBut = document.querySelector('[class*="bottombar_FriendsButton_"]');
-        remAddGamBut.remove();
-        remFriendsBut.remove();
-    };
-
-    const addButtons = async () => {
-        await delay(1000);
-        const divSet = document.createElement("div");
-        divSet.classList.add("steamSettingsButton");
-        divSet.addEventListener("click", function () {location.href = "steam://settings";},false);
-        const divSetIcon = document.createElement("span");
-        divSetIcon.classList.add("steamSettingsIcon");
-        const setIcon = document.createTextNode("");
-        divSetIcon.appendChild(setIcon);
-        divSet.appendChild(divSetIcon);
-
-        const parent = document.querySelector('[class*="bottombar_BottomBar_"]');
-        const child = document.querySelector(
-            '[class*="bottombar_DownloadStatus_"]'
-        );
-        parent.insertBefore(divSet, child);
-    };
-    removeGarbage();
-    addButtons();
-}
+import './src/scripts/main.js';
+import './src/scripts/reloadButton.js';
+import './src/scripts/restartButton.js';
