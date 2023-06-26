133387a878556ff04557eb65b04a509df3af622c
Hexality <victornacano@gmail.com>
2023-06-26 00:47:33 -0300

    updated some stuff, fixed the weird bar on the right

--- libraryroot.custom.js
+++ libraryroot.custom.js
@@ -1,3 +1,6 @@
-import './src/scripts/main.js';
+import './src/scripts/frame.js';
+import './src/scripts/library.js';
+import './src/scripts/navIndicator.js';
 import './src/scripts/reloadButton.js';
 import './src/scripts/restartButton.js';
+console.log('Loading components...');
