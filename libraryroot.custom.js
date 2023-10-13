879408fae2d9d1f1e8af6c17fa98facbc8baf92e
Hexality <victornacano@gmail.com>
2023-10-13 14:41:36 -0300

    rewrite

--- libraryroot.custom.js
+++ libraryroot.custom.js
@@ -1,26 +1,7 @@
-import { waitForElement } from './src/scripts/waitForElement.js';
-import './src/scripts/frame.js';
-import './src/scripts/library.js';
-import './src/scripts/navIndicator.js';
-import './src/scripts/reloadButton.js';
-import './src/scripts/restartButton.js';
-import './src/scripts/disableSmallMode.js';
-window.opener.console.log('Loading components...');
+import './src/scripts/reloadButton.js'
+import './src/scripts/restartButton.js'
 
-const loader = async () => {
-    const curtain = document.createElement('div');
-    const logo = document.createElement('div');
-    curtain.classList.add('loadCurtain');
-    curtain.addEventListener('animationend', () => {
-        curtain.remove();
-    })
-    logo.classList.add('skinLogoBox');
-    curtain.appendChild(logo)
-    const { matchedElements } = await waitForElement('[class*="steamdesktop_FocusNavigationRoot_"]', 1000);
+import { renderer } from './src/scripts/renderer.js';
 
-    matchedElements.forEach((e) => {
-        e.appendChild(curtain);
-    })
-}
-
-loader()
+console.log('Loading components...');
+renderer();
